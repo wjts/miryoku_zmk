bf7ff3eaa960876b24fefb3a73158bfe15f90ac8
Wojciech Wojciechowski <wojtas@wojtas.dev>
2024-06-27 14:24:22 +0200

    WIP: outer column

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -7,10 +7,10 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp LC(F)  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp PSCRN \
-&caps_word  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &sk RALT \
-&kp RALT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp RALT \
-               K32  K33  K34       K35  K36  K37
+&kp LC(F)   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp PSCRN \
+&caps_word  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp LC(A) \
+&sk RALT    K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &sk RALT \
+                      K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
